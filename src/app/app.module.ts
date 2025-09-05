13730a827c01b675cf1e6646ad18022cfed90430
rolyan66@gmail.com <rolyan66@gmail.com>
2025-09-05 10:13:47 -0400

    Se realizo la adición de la estructura de los componentes ademas se adiciono el cdn bootstrap

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ListCitasComponent } from './component/list-citas/list-citas.component';
+import { CrearCitasComponent } from './component/crear-citas/crear-citas.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListCitasComponent,
+    CrearCitasComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
