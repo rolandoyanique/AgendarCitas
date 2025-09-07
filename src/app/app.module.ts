44ec610ea34fac26059c4d21d07b6e77551285df
Rolando <rolyan66@gmail.com>
2025-09-07 18:51:47 -0400

    Se añadio el objeto que envia la informacion del componente agregar cita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ListCitasComponent } from './component/list-citas/list-citas.component';
 import { CrearCitasComponent } from './component/crear-citas/crear-citas.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CrearCitasComponent } from './component/crear-citas/crear-citas.compone
     CrearCitasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/crear-citas/crear-citas.component.ts
+++ src/app/component/crear-citas/crear-citas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 
 @Component({
   selector: 'app-crear-citas',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./crear-citas.component.css']
 })
 export class CrearCitasComponent {
-
+  nombre='';
+  fecha='';
+  hora='';
+  sintomas='';
+  formularioIncorrecto=false;
+  agregarCita(){
+    if(this.nombre=='' || this.fecha==''|| this.hora=='' || this.sintomas==''){
+      this.formularioIncorrecto=true;
+      return;
+    }
+    this.formularioIncorrecto=false;
+    const CITA={
+      nombre:this.nombre,
+      fecha:this.fecha,
+      hora:this.hora,
+      sintomas:this.sintomas
+    }
+    //@Output CITA;
+    this.resetCampos();
+  }
+  resetCampos(){
+    this.nombre='';
+    this.fecha='';
+    this.hora='';
+    this.sintomas='';
+  }
 }
